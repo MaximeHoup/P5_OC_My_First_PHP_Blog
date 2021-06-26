d3c4bc0f734bc22d3af2e6d437fc62163c790d33
MaximeHoup <82955892+MaximeHoup@users.noreply.github.com>
2021-06-26 22:11:30 +0200

    Update addcomment.php

--- model/addcomment.php
+++ model/addcomment.php
@@ -5,3 +5,4 @@ $reponse = $bdd->prepare('INSERT INTO test(nickname, content, posts_id) VALUES(?
 $reponse->execute(array($_POST['nickname'], $_POST['content'], $_GET['post']));
 
 echo $_POST['content'], $_GET['post'], $_POST['nickname'];
+?>
