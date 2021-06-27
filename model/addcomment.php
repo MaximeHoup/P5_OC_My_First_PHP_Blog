0b0228a182a93836ad8c94b4d7919654b42d1644
MaximeHoup <82955892+MaximeHoup@users.noreply.github.com>
2021-06-28 01:47:09 +0200

    Add files via upload

--- model/addcomment.php
+++ model/addcomment.php
@@ -1,8 +1,9 @@
 <?php
 	require_once('dbconnect.php');
 
-$reponse = $bdd->prepare('INSERT INTO test(nickname, content, posts_id) VALUES(?, ?, ?');
+$reponse = $bdd->prepare('INSERT INTO comments(nickname, content, posts_id) VALUES(?, ?, ?)');
 $reponse->execute(array($_POST['nickname'], $_POST['content'], $_GET['post']));
 
-echo $_POST['content'], $_GET['post'], $_POST['nickname'];
+header('Location: ../view/postlist.php');
+
 ?>
