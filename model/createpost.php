17d67437e6fb846a911a374531ca36692b585c9e
MaximeHoup <82955892+MaximeHoup@users.noreply.github.com>
2021-06-28 01:47:30 +0200

    edit values

--- model/createpost.php
+++ model/createpost.php
@@ -2,7 +2,7 @@
 require_once('dbconnect.php');
 session_start();
 $reponse = $bdd->prepare('INSERT INTO posts(title, leadparagraph, content, nickname, users_id) VALUES(?, ?, ?, ?, ?)');
-$reponse->execute(array($_POST['title'], $_POST['leadparagraph'], $_POST['content'], $_POST['content'], $_SESSION['users_id']));
+$reponse->execute(array($_POST['title'], $_POST['leadparagraph'], $_POST['content'], $_POST['nickname'], $_SESSION['users_id']));
 
 header('Location: ../view/postlist.php');
 
