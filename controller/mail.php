259b40a670a298899c1ff2b1768ccc58815a3379
MaximeHoup <82955892+MaximeHoup@users.noreply.github.com>
2021-06-28 15:55:57 +0200

    Add files via upload

--- controller/mail.php
+++ controller/mail.php
@@ -6,6 +6,14 @@
 	</head>
 
 	<body>
-	    <?php echo '<p>Votre message a bien été envoyé.</p>'; ?>
+		<?php $name = $_POST['name'];
+		$email = $_POST['mail'];
+		$content = $_POST['content'];
+		$formcontent="From: $name \n Message: $content";
+		$recipient = "adminp5@gmail.com";
+		$subject = "Contact Form";
+		$mailheader = "From: $email \r\n";
+		mail($recipient, $subject, $formcontent, $mailheader) or die("Erreur! Le message n'a pu être envoyer");
+		echo '<p>Votre message a bien été envoyé.</p>'; ?>
 	</body>
 </html>
