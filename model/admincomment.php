a364ac917e84c092a7df177ad532b2f1ad3795e5
MaximeHoup <82955892+MaximeHoup@users.noreply.github.com>
2021-06-29 19:26:32 +0200

    Add files via upload

--- model/admincomment.php
+++ model/admincomment.php
@@ -1,11 +1,11 @@
 <?php
 require_once('dbconnect.php');
 
-$posts_id = $_GET['post'];
+$comments_id = $_GET['comment'];
 $commentstatut = "1";
 
-$reponse = $bdd->prepare('UPDATE users SET commentaccepted = :accepted WHERE posts_id = :id');
-$reponse->execute(array('id' => $posts_id, 'accepted' => $commentstatut));
+$reponse = $bdd->prepare('UPDATE comments SET commentaccepted = :accepted WHERE comments_id = :id');
+$reponse->execute(array('id' => $comments_id, 'accepted' => $commentstatut));
 
 header('Location: ../view/admincomment.php');
 
