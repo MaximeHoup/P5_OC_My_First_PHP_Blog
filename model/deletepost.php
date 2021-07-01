a5de7c4e876b2d42bfb428cfc6d6ee39fabd576b
MaximeHoup <82955892+MaximeHoup@users.noreply.github.com>
2021-07-01 12:42:09 +0200

    delete also the comments when the post is deleted

--- model/deletepost.php
+++ model/deletepost.php
@@ -3,6 +3,9 @@ require_once('dbconnect.php');
 
 $posts_id = $_GET['post'];
 
+$req = $bdd->prepare('DELETE FROM comments WHERE posts_id = :newposts_id');
+$req->execute(array('newposts_id' => $posts_id));
+
 $reponse = $bdd->prepare('DELETE FROM posts WHERE posts_id = :newposts_id');
 $reponse->execute(array('newposts_id' => $posts_id));
 header('Location: ../view/adminpost.php');
