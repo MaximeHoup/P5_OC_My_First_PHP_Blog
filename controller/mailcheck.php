042adfc70996e92db99aad4ba8df1ef4b27c6336
MaximeHoup <82955892+MaximeHoup@users.noreply.github.com>
2021-07-02 15:40:32 +0200

    Add files via upload

---
+++ controller/mailcheck.php
@@ -0,0 +1,31 @@
+<?php
+    
+$name = valid_donnees($_POST["name"]);
+$mail = valid_donnees($_POST["mail"]);
+$content = valid_donnees($_POST["content"]);
+
+function valid_donnees($donnees){
+    $donnees = trim($donnees);
+    $donnees = stripslashes($donnees);
+    $donnees = htmlspecialchars($donnees);
+    return $donnees;
+}
+
+/*Si les champs prenom et mail ne sont pas vides et si les donnees ont
+ *bien la forme attendue...*/
+if (!empty($name)
+    && strlen($name) <= 20
+    && preg_match("^[A-Za-z '-]+$",$name)
+    && !empty($mail)
+    && filter_var($mail, FILTER_VALIDATE_EMAIL)){
+
+    try{
+        include ("mail.php");
+        }
+        catch(PDOException $e){
+            echo 'Erreur : '.$e->getMessage();
+        }
+    }else{
+        echo 'Données invalides';
+    }
+?>
